16baa8a1df174f01c54722169f1f063a3024851c
Zhiyun Ling <chihyunling@gmail.com>
2024-01-09 20:23:09 -0600

    hello_world&hello_cargo

---
+++ projects/hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // ! means a macro
+    // Indent with 4 spaces, not tab
+    // rustc main.rs to compile
+    // rustfmt main.rs to format using standard formatter
+    // Most lines end with semicolon ;
+    println!("hello, world!");
+}
